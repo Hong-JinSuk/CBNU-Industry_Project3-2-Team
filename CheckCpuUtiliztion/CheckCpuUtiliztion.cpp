9dccfb0415c790b9773f95eef276c2d8eae30fcb
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2022-10-15 13:59:27 +0900

    Update CheckCpuUtiliztion.cpp

--- CheckCpuUtiliztion/CheckCpuUtiliztion.cpp
+++ CheckCpuUtiliztion/CheckCpuUtiliztion.cpp
@@ -1,16 +1,35 @@
+#pragma comment(lib, "pdh.lib")
 #include <iostream>
 #include <Pdh.h>
 
 using namespace std;
 
-float getCpu() {
+// get current cpu
+static PDH_HQUERY cpuQuery;
+static PDH_HCOUNTER cpuTotal;
 
+
+void init() {
+	PdhOpenQuery(NULL, NULL, &cpuQuery);
+	PdhAddCounter(cpuQuery, L"\\Processor(_Total)\\% Processor Time", NULL, &cpuTotal);
+	PdhCollectQueryData(cpuQuery);
+}
+
+float getCpuUsage() {
+	
+	// operator definition
+	PDH_FMT_COUNTERVALUE counterVal;
+	
+	// get cpuQuery and find value
+	PdhCollectQueryData(cpuQuery);
+	PdhGetFormattedCounterValue(cpuTotal, PDH_FMT_DOUBLE, NULL, &counterVal);
+	return counterVal.doubleValue;
 }
 
 void printCpu() {
 	while (true) {
 
-		cout << "Cpu Usage : " << getCpu() << endl;
+		cout << "Cpu Usage : " << getCpuUsage() << endl;
 		Sleep(1000);
 
 		/*if (shutdownCom) {
@@ -21,6 +40,8 @@ void printCpu() {
 }
 
 int main() {
+	
+	init();
 
 	printCpu();
 
