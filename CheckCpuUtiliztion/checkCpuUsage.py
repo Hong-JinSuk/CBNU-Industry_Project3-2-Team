59502f5d969c5505e36ae181484d080849c7e153
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2022-11-28 15:13:53 +0900

    Update checkCpuUsage.py

--- CheckCpuUtiliztion/checkCpuUsage.py
+++ CheckCpuUtiliztion/checkCpuUsage.py
@@ -5,6 +5,7 @@ def checkCpuUsage(PID):
     for process in psutil.process_iter():
         for pid in PID:
             if str(process.pid) == str(pid):
-                print("CpuUsage of {} : ".format(process.name()) + str(process.cpu_percent(interval=1)))
+                #cpu_percent에서 검사결과가 미미한 수준인경우 0.0으로 표시된다.
+                print("CpuUsage of {0}({1}) : ".format(process.name(), process.pid) + str(process.cpu_percent(interval=1)))
 
 checkCpuUsage(List)
