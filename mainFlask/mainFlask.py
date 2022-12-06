40029ba59dc5983ac55f7b025919bae5f85e72d6
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2022-12-06 16:26:56 +0900

    Update mainFlask.py

--- mainFlask/mainFlask.py
+++ mainFlask/mainFlask.py
@@ -17,3 +17,11 @@ def set_time():
         #if remained_time==0, kill process
 
     return
+
+if __name__ == '__main__':
+    remained_time = 300 # 관리자가 설정할 시간
+
+    thread = Thread(target=set_time, daemon=True)
+    thread.start()
+
+    app.run(debug=True)
