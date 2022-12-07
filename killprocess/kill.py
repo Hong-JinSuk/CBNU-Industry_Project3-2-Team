deb1dab04e27525ecfc11b209f705603dcde6718
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-07 16:45:07 +0900

    Create kill.py

---
+++ killprocess/kill.py
@@ -0,0 +1,18 @@
+import psutil
+ 
+for proc in psutil.process_iter():
+    try:
+        # 프로세스 이름, PID값 가져오기
+        processName = proc.name()
+        processID = proc.pid
+        print(processName , ' - ', processID)
+ 
+        if processName == "notepad.exe":
+            parent_pid = processID  #PID
+            parent = psutil.Process(parent_pid) # PID 찾기
+            for child in parent.children(recursive=True):  #자식-부모 종료
+                child.kill()
+            parent.kill()
+ 
+    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):   #예외처리
+        pass
