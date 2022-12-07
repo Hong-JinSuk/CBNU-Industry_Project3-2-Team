dd7178a4004f321be61bda67abdc2a5a0a510acb
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2022-12-07 22:03:36 +0900

    Update mainFlask.py

--- mainFlask/mainFlask.py
+++ mainFlask/mainFlask.py
@@ -41,6 +41,24 @@ def findProcessName(processName):
     
     return listOfProcess
 
+def killprocess(PID):
+    for proc in psutil.process_iter():
+        try:
+            # 프로세스 이름, PID 값 가져오기
+            processName = proc.name()
+            processID = proc.pid
+
+            for i in PID:
+                if processID == i:
+                    parent_pid = processID  #PID
+                    parent = psutil.Process(parent_pid)  #process 찾기
+                    for child in parent.children(recursive=True):  #자식-부모 종료
+                        child.kill()
+                    parent.kill()
+
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):   #예외처리
+            pass
+  
 if __name__ == '__main__':
     remained_time = 300 # 관리자가 설정할 시간
 
