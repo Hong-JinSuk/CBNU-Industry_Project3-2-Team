a26d1dc5518854eb1640a2c39410ee7c93aa1baa
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-07 17:27:17 +0900

    Update kill.py

--- killprocess/kill.py
+++ killprocess/kill.py
@@ -1,13 +1,13 @@
 def killprocess(List, PID):
     for proc in psutil.process_iter():
-    try:
-        for i in PID:
-            if List[i] == i:
-                parent_pid = List[i]  #PID
-                parent = psutil.Process(parent_pid)  #process 찾기
-                for child in parent.children(recursive=True):  #자식-부모 종료
-                    child.kill()
-                parent.kill()
- 
-    except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):   #예외처리
-        pass
+        try:
+            for i in PID:
+                if List[i] == i:
+                    parent_pid = List[i]  #PID
+                    parent = psutil.Process(parent_pid)  #process 찾기
+                    for child in parent.children(recursive=True):  #자식-부모 종료
+                        child.kill()
+                    parent.kill()
+
+        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):   #예외처리
+            pass
