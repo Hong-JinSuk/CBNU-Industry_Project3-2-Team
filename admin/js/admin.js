e895112dae63ce86eedd3acb4b42cdb86e8ba682
ByeongJun-Jang <85213981+ByeongJun-Jang@users.noreply.github.com>
2022-12-07 16:37:53 +0900

    Update admin.js

--- admin/js/admin.js
+++ admin/js/admin.js
@@ -1,3 +1,11 @@
+window.onscroll=function(){
+    scrollRotate();
+};
+function scrollRotate(){
+    let image = document.getElementById("reload");
+    image.style.transform = "rotate(" + window.pageYOffset/2 + "deg)";
+}
+
 function print_time(){
     var userda = document.getElementById('user_data');
     var hourda = document.getElementById('hour_data');
