f49c797529a9e23a0f7d58c1ec65489736f2eba1
ByeongJun-Jang <85213981+ByeongJun-Jang@users.noreply.github.com>
2022-12-07 15:52:01 +0900

    Create admin.js

---
+++ admin/js/admin.js
@@ -0,0 +1,10 @@
+function print_time(){
+    var userda = document.getElementById('user_data');
+    var hourda = document.getElementById('hour_data');
+    var minda = document.getElementById('min_data');
+    
+    if(userda != null && hourda != null && minda != null){
+        alert(userda.value + ' 번 사용자 ' + hourda.value + ' 시간 ' + minda.value + ' 분 설정을 완료하였습니다 !');
+    }
+    
+}
