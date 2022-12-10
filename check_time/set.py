c857a75ae894ed9ee3baa2c381ff1a41d3ea5a78
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-10 22:01:30 +0900

    Update set.py

--- check_time/set.py
+++ check_time/set.py
@@ -5,7 +5,7 @@ df=pd.DataFrame([[0,120],
                  [0,60],
                  [0,30]],
                index=['user1','user2','user3'],
-                columns=['사용시간(분)','제한시간(분)'])
+                columns=['사용시간','제한시간'])
 
 #파일로 저장
 df.to_csv('파일 경로',header='False',encoding='utf-8-sig')
