eb7d630b11db3cdda996e5974b49ccf985109767
Hong-JinSuk <86042721+Hong-JinSuk@users.noreply.github.com>
2022-12-10 18:33:16 +0900

    Update mainFlask.py

--- mainFlask/mainFlask.py
+++ mainFlask/mainFlask.py
@@ -72,10 +72,25 @@ def checkCpuUsage_thread(pid):
             if str(process.pid) == str(pid):
                 Cpu_Per=process.cpu_percent(interval=10)
                 list_cpu_percent.append(Cpu_Per)
-                print("CpuUsage of {0}({1}) : ".format(process.name(), process.pid)+ " " +str(Cpu_Per))
+                #print("CpuUsage of {0}({1}) : ".format(process.name(), process.pid)+ " " +str(Cpu_Per))
         time.sleep(10)
         list_cpu_percent.clear()
-
+        
+def checkMemoryUsage(PID):
+    while True:
+        for pid in PID:
+            #print("--"*30)
+            #print(pid)
+            # current process RAM usage
+            total_memory = psutil.virtual_memory()
+            print(total_memory[2]) #psutil.virtual_memory()는 3번째 원소에 메모리 사용률을 반환함.
+            
+            # pid별 메모리 사용률을 받아와야함.
+            current_process = psutil.Process(pid)
+            current_process_memory_usage_as_GB = current_process.memory_info()[0] / 3.**20
+            print(f"Current memory GB   : {current_process_memory_usage_as_GB: 9.3f} GB")
+        time.sleep(10)
+        
 if __name__ == '__main__':
     
     global remained_time
