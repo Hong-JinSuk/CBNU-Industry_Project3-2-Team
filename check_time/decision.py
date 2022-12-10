3fe31ad90d8516147e011041a359194e3849ac3a
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-10 16:30:37 +0900

    Update decision.py

--- check_time/decision.py
+++ check_time/decision.py
@@ -1,3 +1,13 @@
 if "게임 중":
     #파일 읽어오기
     in_df=pd.read_csv('파일 경로',index_col=0,engine='python')
+    
+    time_used=in_df.loc['user1','사용시간']
+    time_limit=in_df.loc['user1','제한시간']
+    
+    #사용시간 > 제한시간 이면 게임 강제 종료
+    #그 외에 정상적으로 진행
+    while True:
+        if time_used>time_limit:
+            killprocess(PID)
+            break
