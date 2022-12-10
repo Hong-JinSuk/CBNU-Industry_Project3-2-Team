8dd3c48e41b330abdcb5fb7f3c7dbcd6dbf082f8
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-10 17:50:38 +0900

    Update graph.js

--- admin/admin_check/js/graph.js
+++ admin/admin_check/js/graph.js
@@ -3,3 +3,10 @@ var series = ["user1", "user2","user3"];
 var dataset = [ 
     {'월':116, '화':159, '수':134, '목':27, '금':55, '토':24,  '일':45},
     {'월':17, '화':27, '수':37, '목':27, '금':17, '토':7,  '일':9}];
+
+var keys = d3.keys(dataset[0]);
+var data = [];
+
+dataset.forEach(function(d, i) {
+data[i] = keys.map(function(key) { return {x: key, y: d[key]}; })
+});
