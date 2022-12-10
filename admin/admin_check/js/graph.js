f170791bf0549dfd1a7abd6d6173bb297d43f639
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-10 17:51:48 +0900

    Update graph.js

--- admin/admin_check/js/graph.js
+++ admin/admin_check/js/graph.js
@@ -14,3 +14,6 @@ data[i] = keys.map(function(key) { return {x: key, y: d[key]}; })
 var margin = {left: 20, top: 10, right: 10, bottom: 20};
 var svg = d3.select("svg");
 var width  = parseInt(svg.style("width"), 10) - margin.left - margin.right;
+var height = parseInt(svg.style("height"), 10)- margin.top  - margin.bottom;
+var svgG = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
