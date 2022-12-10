324d0a11c3d86357a234a054730f2448077805dd
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-10 21:33:47 +0900

    Update alarm_win32api.py

--- check_time/alarm_win32api.py
+++ check_time/alarm_win32api.py
@@ -3,7 +3,9 @@ import pandas as pd
 
 df=pd.read_csv('파일 경로',index_col=0,engine='python')
 time_used=df.loc['user1','사용시간']
+time_limit=in_df.loc['user1','제한시간']
+remain=time_limit-time_used  #남은 시간
 
 #바로 알림창 띄우기
-if time_used==10:
+if remain==10:
     win32api.MessageBox(0, "10분 남았습니다.", "알림", 48)
