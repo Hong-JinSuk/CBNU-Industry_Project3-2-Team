70cb985ea0c656d3af9f56875e5d68da59c98b5d
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-10 18:33:52 +0900

    Update percent.js

--- time_visualization/percent.js
+++ time_visualization/percent.js
@@ -11,3 +11,9 @@ const makeChart = (percent, classname, color) => {
     }
   }, 10);
 }
+
+const colorFn = (i, classname, color) => {
+  classname.style.background = "conic-gradient(" + color + " 0% " + i + "%, #dedede " + i + "% 100%)";
+}
+
+makeChart(75, chart1, '#f5b914');
