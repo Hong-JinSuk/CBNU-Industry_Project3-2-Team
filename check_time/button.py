941de25d33f4eca834720cde6df9c59c73e038d9
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-10 21:01:50 +0900

    Update button.py

--- check_time/button.py
+++ check_time/button.py
@@ -8,3 +8,10 @@ class Ui_MainWindow(QtWidgets.QWidget):
 
         self.centralwidget = QtWidgets.QWidget(self)
         self.centralwidget.setObjectName("centralwidget")
+
+        # Information_button
+        self.Information_button = QtWidgets.QPushButton(self.centralwidget)
+        self.Information_button.setGeometry(QtCore.QRect(140, 90, 100, 30))
+        self.Information_button.setObjectName("Information_button")
+        self.Information_button.setText("Information")
+        self.Information_button.clicked.connect(self.Information_event)
