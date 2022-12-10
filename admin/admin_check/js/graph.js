289f033f9533a419f901aaec340e3398bd69097b
kgeunwo77 <79292371+kgeunwo77@users.noreply.github.com>
2022-12-10 17:52:05 +0900

    Update graph.js

--- admin/admin_check/js/graph.js
+++ admin/admin_check/js/graph.js
@@ -17,3 +17,6 @@ var width  = parseInt(svg.style("width"), 10) - margin.left - margin.right;
 var height = parseInt(svg.style("height"), 10)- margin.top  - margin.bottom;
 var svgG = svg.append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var xScale = d3.scalePoint()//scaleBand() scaleOrdinal
+    .domain(keys)
+    .rangeRound([0, width]);
